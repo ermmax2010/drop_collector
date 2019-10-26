3a2eb0ee858abfe48d59ef5358aa14ef1920ffcf
Yermalayeu Ihar <ermig@tut.by>
2019-10-26 18:49:13 +0300

    +add drop_collector version-0.1.

---
+++ drop_collector/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(188, 449);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(415, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(26, 62);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(441, 365);
+            this.panel1.TabIndex = 4;
+            this.panel1.Click += new System.EventHandler(this.panel1_Click);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 515);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ drop_collector/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Text = "Play";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        struct Drop
+        {
+            public bool exist;
+            public int X, Y, R;
+        }
+
+        struct Backet
+        {
+            public int X, Y, W, H;
+        }
+
+        Drop[] drops = new Drop[12];
+        int lives, score;
+        Backet backet = new Backet();
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < drops.Length; i = i + 1)
+            {
+                drops[i].Y = drops[i].Y + 6;
+                if(drops[i].Y > backet.Y && drops[i].exist)
+                {
+                    if (drops[i].X > backet.X && drops[i].X < backet.X + backet.W)
+                    {
+                        score = score + 1;
+                    }
+                    else
+                    {
+                        lives = lives - 1;
+                    }
+                    drops[i].exist = false;
+                }
+            }
+            panel1.Invalidate();
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            backet.X = e.X;
+            panel1.Invalidate();
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            label1.Text = String.Format("Score = {0}", score);
+            label2.Text = String.Format("Lives = {0}", lives);
+            if(lives <= 0)
+            {
+                e.Graphics.DrawString("Yoy are lose!", DefaultFont, new SolidBrush(Color.Red), 100, 100);
+                return;
+            }
+            if (score >= 10)
+            {
+                e.Graphics.DrawString("You are wine!", DefaultFont, new SolidBrush(Color.Green), 100, 100);
+                return;
+            }
+            for (int i = 0; i < drops.Length; i = i + 1)
+            {
+                if(drops[i].exist)
+                    e.Graphics.DrawEllipse(new Pen(Color.Blue), new Rectangle(drops[i].X - drops[i].R, drops[i].Y - drops[i].R, 2 * drops[i].R, 2 * drops[i].R));
+            }
+            e.Graphics.DrawRectangle(new Pen(Color.Black), new Rectangle(backet.X, backet.Y, backet.W, backet.H));
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lives = 3;
+            score = 0;
+
+            backet.X = 200;
+            backet.Y = 250;
+            backet.W = 100;
+            backet.H = 100;
+
+
+            Random random = new Random(0);
+            for(int i = 0, y = 0; i < drops.Length; i = i + 1)
+            {
+                drops[i].exist = true;
+                drops[i].X = (int)Math.Round(random.NextDouble()*(panel1.Width - 10));
+                y = y - 50 - (int)Math.Round(random.NextDouble() * 100);
+                drops[i].Y = y;
+                drops[i].R = 10;
+            }
+
+            timer1.Start();
+            panel1.Invalidate();
+        }   
+    }
+}
